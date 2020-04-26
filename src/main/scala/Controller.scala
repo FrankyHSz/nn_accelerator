8477977a17e456ae4cc11284f35b5c26ad26baaa
Szabolcs Hornyák <sztarkantal@gmail.com>
2020-04-26 20:30:34 +0200

    Controller v0.2: created FSM that operates on dummy commands

--- src/main/scala/Controller.scala
+++ src/main/scala/Controller.scala
@@ -1,9 +1,13 @@
 
 import chisel3._
-import chisel3.util.{Cat, log2Up}
+import chisel3.util.{Cat, Enum, log2Up}
 import _root_.memory._
 
 class Controller(testInternals: Boolean) extends Module {
+
+  // --------------------
+  // Module configuration
+  // --------------------
 
   // Register address map
   val STATUS    = 0 * busDataWidth + 0
@@ -11,10 +15,10 @@ class Controller(testInternals: Boolean) extends Module {
   val LD_ADDR_B = 2 * busDataWidth
   val LD_SIZE_B = 3 * busDataWidth
 
-  // Control/status register bits (indices) -- RD: readable, WR: writable, CL: cleanable
+  // Control/status register bits (indices) -- RD: readable, WR: writable, CL: cleanable, ST: settable
   val chEn = 0  // Chip enable      : enables operation according to the command queue (RD/WR)
   val busy = 1  // Busy flag        : signals that operations are under execution      (RD)
-  val qEmp = 2  // Queue is empty   : signals that no operation is specified           (RD)
+  val qEmp = 2  // Queue is empty   : signals that no operation is specified           (RD/ST)
   val itEn = 3  // Interrupt enable : enables generation of processor interrupts       (RD/WR)
   val itFl = 4  // Interrupt flag   : signals processor interrupt (if enabled)         (RD/CL)
   val acEn = 5  // Activation enable: if cleared, bypasses the Activation Grid         (RD/WR)
@@ -24,7 +28,14 @@ class Controller(testInternals: Boolean) extends Module {
   val inactiveBitsInStatus = busDataWidth - 7
 
   // Useful constants for module
-  val blockAddrW = log2Up(busDataWidth)
+  val blockAddrW  = log2Up(busDataWidth)
+  val numOfStates = 4
+  val stateRegW   = log2Up(numOfStates)
+
+
+  // ----------------
+  // Module interface
+  // ----------------
 
   val io = IO(new Bundle {
 
@@ -41,28 +52,39 @@ class Controller(testInternals: Boolean) extends Module {
     val ldSizeSel  = Input(Bool())
 
     // Test port
+    // - Memory-mapped and internal registers
     val statusReg  = if (testInternals) Output(UInt(busDataWidth.W)) else Output(UInt(0.W))
-    val cmdValid   = if (testInternals) Output(UInt(busDataWidth.W)) else Output(UInt(0.W))
+    val cmdValid   = if (testInternals) Output(Vec(busDataWidth, Bool())) else Output(Vec(0, Bool()))
     val commandRF  = if (testInternals) Output(Vec(busDataWidth, UInt(busDataWidth.W))) else Output(Vec(0, UInt(0.W)))
-    val ldAValid   = if (testInternals) Output(UInt(busDataWidth.W)) else Output(UInt(0.W))
-    val ldSValid   = if (testInternals) Output(UInt(busDataWidth.W)) else Output(UInt(0.W))
+    val ldAValid   = if (testInternals) Output(Vec(busDataWidth, Bool())) else Output(Vec(0, Bool()))
+    val ldSValid   = if (testInternals) Output(Vec(busDataWidth, Bool())) else Output(Vec(0, Bool()))
     val loadAddrRF = if (testInternals) Output(Vec(busDataWidth, UInt(busDataWidth.W))) else Output(Vec(0, UInt(0.W)))
     val loadSizeRF = if (testInternals) Output(Vec(busDataWidth, UInt(busDataWidth.W))) else Output(Vec(0, UInt(0.W)))
+    // - Registers of state machine
+    val stateReg    = if (testInternals) Output(UInt(stateRegW.W)) else Output(UInt(0.W))
+    val cmdPtr      = if (testInternals) Output(UInt(blockAddrW.W)) else Output(UInt(0.W))
+    val ldAPtr      = if (testInternals) Output(UInt(blockAddrW.W)) else Output(UInt(0.W))
+    val ldSPtr      = if (testInternals) Output(UInt(blockAddrW.W)) else Output(UInt(0.W))
+    val currCommand = if (testInternals) Output(UInt(busDataWidth.W)) else Output(UInt(0.W))
   })
+
+
+  // ------------------------------------
+  // Memory-mapped and internal registers
+  // ------------------------------------
 
   // Control/status register
   val statusReg = RegInit(0.U(busDataWidth.W))
 
   // Command registers
-  val cmdValid  = RegInit(0.U(busDataWidth.W))  // Internal, not memory-mapped
+  val cmdValid  = RegInit(VecInit(Seq.fill(busDataWidth)(false.B)))  // Internal, not memory-mapped
   val commandRF = Reg(Vec(busDataWidth, UInt(busDataWidth.W)))
 
   // Load registers
-  val ldAValid   = RegInit(0.U(busDataWidth.W))  // Internal, not memory-mapped
-  val ldSValid   = RegInit(0.U(busDataWidth.W))  // Internal, not memory-mapped
+  val ldAValid   = RegInit(VecInit(Seq.fill(busDataWidth)(false.B)))  // Internal, not memory-mapped
+  val ldSValid   = RegInit(VecInit(Seq.fill(busDataWidth)(false.B)))  // Internal, not memory-mapped
   val loadAddrRF = Reg(Vec(busDataWidth, UInt(busDataWidth.W)))
   val loadSizeRF = Reg(Vec(busDataWidth, UInt(busDataWidth.W)))
-
 
   // Register RD/WR logic
   io.rdData := 0.U
@@ -72,27 +94,67 @@ class Controller(testInternals: Boolean) extends Module {
     } .otherwise {
       val itFlag   = statusReg(itFl) && io.wrData(itFl)
       val bits5to3 = Cat(io.wrData(acEn), itFlag, io.wrData(itEn))
-      val bits5to0 = Cat(bits5to3, statusReg(2, 1), io.wrData(chEn))
+      val queueEmpty = statusReg(qEmp) || io.wrData(qEmp)
+      val bits5to0 = Cat(bits5to3, queueEmpty, statusReg(busy), io.wrData(chEn))
       statusReg := Cat(statusReg(erFl), 0.U(inactiveBitsInStatus.W), bits5to0)
     }
+  } .otherwise {  // Non-bus operations involving statusReg
+    // Refreshing queue-is-empty bit at every clock cycle other than bus reads/writes
+    statusReg := Cat(statusReg(busDataWidth-1, 3), !cmdValid.asUInt.orR, statusReg(1, 0))
   }
   when(io.commandSel) {  // Command register file: WR
     when(!io.rdWrN) {
       commandRF(io.addr(blockAddrW - 1, 0)) := io.wrData
-      cmdValid := Cat(cmdValid(busDataWidth - 2, 0), 1.U(1.W))
     }
   }
   when(io.ldAddrSel) {   // Load Address register file: WR
     when(!io.rdWrN) {
       loadAddrRF(io.addr(blockAddrW - 1, 0)) := io.wrData
-      ldAValid := Cat(ldAValid(busDataWidth - 2, 0), 1.U(1.W))
     }
   }
   when(io.ldSizeSel) {   // Load Size register file: WR
     when(!io.rdWrN) {
       loadSizeRF(io.addr(blockAddrW - 1, 0)) := io.wrData
-      ldSValid := Cat(ldSValid(busDataWidth - 2, 0), 1.U(1.W))
     }
+  }
+
+  // Read pointers
+  val cmdPtr = RegInit(0.U(blockAddrW.W))
+  val ldAPtr = RegInit(0.U(blockAddrW.W))
+  val ldSPtr = RegInit(0.U(blockAddrW.W))
+
+  // Invalidate-on-read signals
+  val cmdRead = WireDefault(false.B)
+  val ldARead = WireDefault(false.B)
+  val ldSRead = WireDefault(false.B)
+
+  // Validity registers
+  // - Writing a word makes that word valid
+  // - Setting the queue-empty bit in statusReg makes every word invalid
+  // - Reading a word makes that word invalid
+  when (io.commandSel && !io.rdWrN) {
+    cmdValid(io.addr(blockAddrW - 1, 0)) := true.B
+  } .elsewhen (io.statusSel && !io.rdWrN && io.wrData(qEmp)) {
+    for (i <- 0 until busDataWidth)
+      cmdValid(i) := false.B
+  } .elsewhen (cmdRead) {
+    cmdValid(cmdPtr) := false.B
+  }
+  when (io.ldAddrSel && !io.rdWrN) {
+    ldAValid(io.addr(blockAddrW - 1, 0)) := true.B
+  } .elsewhen (io.statusSel && !io.rdWrN && io.wrData(qEmp)) {
+    for (i <- 0 until busDataWidth)
+      ldAValid(i) := false.B
+  } .elsewhen (ldARead) {
+    ldAValid(ldAPtr) := false.B
+  }
+  when (io.ldSizeSel && !io.rdWrN) {
+    ldSValid(io.addr(blockAddrW - 1, 0)) := true.B
+  } .elsewhen (io.statusSel && !io.rdWrN && io.wrData(qEmp)) {
+    for (i <- 0 until busDataWidth)
+      ldSValid(i) := false.B
+  } .elsewhen (ldSRead) {
+    ldSValid(ldSPtr) := false.B
   }
 
   // Providing information about the internal
@@ -108,6 +170,69 @@ class Controller(testInternals: Boolean) extends Module {
   }
 
 
+  // ------------------
+  // FSM to operate NNA
+  // ------------------
+
+  // States
+  val idle :: fetch :: decode :: execute :: Nil = Enum(numOfStates)
+
+  // State register
+  val stateReg = RegInit(idle)
+
+  // Command ("instruction") and operand registers (to be continued...)
+  val currCommand = RegInit(0.U(busDataWidth.W))
+
+  // Next state logic and related operations
+  when (stateReg === idle) {
+    when(statusReg(chEn) && !statusReg(qEmp)) {
+      stateReg := fetch
+      cmdPtr := 0.U
+    }
+  } .elsewhen (stateReg === fetch) {
+    when (statusReg(chEn)) {
+      when(cmdValid(cmdPtr)) {
+        stateReg := decode
+        currCommand := commandRF(cmdPtr)
+        cmdPtr := cmdPtr + 1.U
+      } .otherwise {
+        stateReg := idle
+        cmdPtr := 0.U
+      }
+    } .otherwise {
+      stateReg := idle
+      cmdPtr := 0.U
+    }
+  } .elsewhen (stateReg === decode) {
+    when (statusReg(chEn)) {
+      stateReg := execute
+    } .otherwise {
+      stateReg := idle
+    }
+  } .elsewhen (stateReg === execute) {
+    when (cmdValid.asUInt.orR) {
+      stateReg := fetch
+    } .otherwise {
+      stateReg := idle
+    }
+  } .otherwise {
+    stateReg := idle
+  }
+
+  // Signals to invalidate register data on read (to be continued...)
+  cmdRead := (stateReg === fetch) && statusReg(chEn) && cmdValid(cmdPtr)
+
+  // Providing information about the internal
+  // state of Controller for testing
+  if (testInternals) {
+    io.stateReg    := stateReg
+    io.cmdPtr      := cmdPtr
+    io.ldAPtr      := ldAPtr
+    io.ldSPtr      := ldSPtr
+    io.currCommand := currCommand
+  }
+
+
   // Helper functions
   def getRegMap = Map(
     "Status Register"                         -> STATUS,
@@ -115,4 +240,5 @@ class Controller(testInternals: Boolean) extends Module {
     "Load Address Register File Base Address" -> LD_ADDR_B,
     "Load Size Register File Base Address"    -> LD_SIZE_B
   )
+  def getDataW = busDataWidth
 }
--- src/test/scala/CtrlTester.scala
+++ src/test/scala/CtrlTester.scala
@@ -15,10 +15,10 @@ class CtrlTester(dut: Controller) extends PeekPokeTester(dut) {
   poke(dut.io.statusSel, true.B)   // Important
   poke(dut.io.addr, dut.STATUS.U)  // Optional
   // - Write
-  // Sent     Word: the 2 least significant bytes are set to 1
+  // Sent     Word: the 2 least significant bytes are set to 1 (except chip enable)
   // Received Word: only writable bits will change
-  val sentWord = (1 << 16) - 1
-  val recvWord = (1 << dut.chEn) + (1 << dut.itEn) + (1 << dut.acEn)
+  val sentWord = (1 << 16) - 2
+  val recvWord = (1 << dut.qEmp) + (1 << dut.itEn) + (1 << dut.acEn)
   poke(dut.io.wrData, sentWord.U)
   poke(dut.io.rdWrN, false.B)
   expect(dut.io.statusReg, 0.U)
@@ -40,20 +40,23 @@ class CtrlTester(dut: Controller) extends PeekPokeTester(dut) {
     poke(dut.io.addr, (dut.CMD_RF_B + i).U) // Important
     poke(dut.io.wrData, (42 + i).U)
     expect(dut.io.commandRF(i), 0.U)
-    expect(dut.io.cmdValid, cmdValidRef.U)
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.cmdValid(bit), (cmdValidRef.U)(bit))
     step(1)
     cmdValidRef = (cmdValidRef << 1) + 1
     expect(dut.io.commandRF(i), (42 + i).U)
-    expect(dut.io.cmdValid, cmdValidRef.U)
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.cmdValid(bit), (cmdValidRef.U)(bit))
   }
   // - Read back: expecting 0s because these registers are write-only
   poke(dut.io.rdWrN, true.B)
   for (i <- 0 until 5) {
     poke(dut.io.addr, (dut.CMD_RF_B + i).U)
     step(1)
-    expect(dut.io.cmdValid, cmdValidRef)   // Registers are still
-    expect(dut.io.commandRF(i), (42 + i).U)  // holding their values
-    expect(dut.io.rdData, 0.U)             // but rdData is 0
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.cmdValid(bit), (cmdValidRef.U)(bit))  // Registers are still
+    expect(dut.io.commandRF(i), (42 + i).U)               // holding their values
+    expect(dut.io.rdData, 0.U)                            // but rdData is 0
   }
   // Don't forget to deactivate register select after test
   poke(dut.io.commandSel, false.B)
@@ -67,20 +70,23 @@ class CtrlTester(dut: Controller) extends PeekPokeTester(dut) {
     poke(dut.io.addr, (dut.LD_ADDR_B + i).U) // Important
     poke(dut.io.wrData, (42 + i).U)
     expect(dut.io.loadAddrRF(i), 0.U)
-    expect(dut.io.ldAValid, ldAddrValidRef.U)
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.ldAValid(bit), (ldAddrValidRef.U)(bit))
     step(1)
     ldAddrValidRef = (ldAddrValidRef << 1) + 1
     expect(dut.io.loadAddrRF(i), (42 + i).U)
-    expect(dut.io.ldAValid, ldAddrValidRef.U)
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.ldAValid(bit), (ldAddrValidRef.U)(bit))
   }
   // - Read back: expecting 0s because these registers are write-only
   poke(dut.io.rdWrN, true.B)
   for (i <- 0 until 5) {
     poke(dut.io.addr, (dut.LD_ADDR_B + i).U)
     step(1)
-    expect(dut.io.ldAValid, ldAddrValidRef)   // Registers are still
-    expect(dut.io.loadAddrRF(i), (42 + i).U)    // holding their values
-    expect(dut.io.rdData, 0.U)                // but rdData is 0
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.ldAValid(bit), (ldAddrValidRef.U)(bit))  // Registers are still
+    expect(dut.io.loadAddrRF(i), (42 + i).U)                 // holding their values
+    expect(dut.io.rdData, 0.U)                               // but rdData is 0
   }
   // Don't forget to deactivate register select after test
   poke(dut.io.ldAddrSel, false.B)
@@ -94,21 +100,117 @@ class CtrlTester(dut: Controller) extends PeekPokeTester(dut) {
     poke(dut.io.addr, (dut.LD_SIZE_B + i).U) // Important
     poke(dut.io.wrData, (42 + i).U)
     expect(dut.io.loadSizeRF(i), 0.U)
-    expect(dut.io.ldSValid, ldSizeValidRef.U)
+    for (j <- 0 until dut.getDataW)
+      expect(dut.io.ldSValid(i), (ldSizeValidRef.U)(i))
     step(1)
     ldSizeValidRef = (ldSizeValidRef << 1) + 1
     expect(dut.io.loadSizeRF(i), (42 + i).U)
-    expect(dut.io.ldSValid, ldSizeValidRef.U)
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.ldSValid(bit), (ldSizeValidRef.U)(bit))
   }
   // - Read back: expecting 0s because these registers are write-only
   poke(dut.io.rdWrN, true.B)
   for (i <- 0 until 5) {
     poke(dut.io.addr, (dut.LD_SIZE_B + i).U)
     step(1)
-    expect(dut.io.ldSValid, ldSizeValidRef)  // Registers are still
-    expect(dut.io.loadSizeRF(i), (42 + i).U)   // holding their values
-    expect(dut.io.rdData, 0.U)               // but rdData is 0
+    for (bit <- 0 until dut.getDataW)
+      expect(dut.io.ldSValid(bit), (ldSizeValidRef.U)(bit))  // Registers are still
+    expect(dut.io.loadSizeRF(i), (42 + i).U)                 // holding their values
+    expect(dut.io.rdData, 0.U)                               // but rdData is 0
   }
   // Don't forget to deactivate register select after test
   poke(dut.io.ldSizeSel, false.B)
+
+
+  // State machine tests
+  // -------------------
+  println("[CtrlTester] State machine testing...")
+
+  // Clearing all enable signals and flags
+  // - resetWord: Everything is 0 except queue-empty bit which invalidates every command
+  val resetWord = (1 << dut.qEmp)
+  poke(dut.io.statusSel, true.B)
+  poke(dut.io.wrData, resetWord.U)
+  poke(dut.io.rdWrN, false.B)
+  step(1)
+  expect(dut.io.statusReg, resetWord)
+  poke(dut.io.statusSel, false.B)
+
+  // Writing commands
+  val dummyCmd = 42
+  poke(dut.io.commandSel, true.B)
+  poke(dut.io.rdWrN, false.B)
+  poke(dut.io.addr, 0.U)
+  poke(dut.io.wrData, dummyCmd.U)
+  // - Before clock edge, everything remains in reset
+  expect(dut.io.statusReg, resetWord)
+  for (i <- 0 until dut.getDataW)
+    expect(dut.io.cmdValid(i), false)
+  step(1)
+  // - After first clock edge, only cmdValid(0) changes to true
+  expect(dut.io.statusReg, resetWord)
+  for (i <- 0 until dut.getDataW)
+    expect(dut.io.cmdValid(i), (i==0))
+  step(1)
+  // - After second clock edge, statusReg's queue-empty bit changes to 0
+  expect(dut.io.statusReg, 0.U)
+  for (i <- 0 until dut.getDataW)
+    expect(dut.io.cmdValid(i), (i==0))
+  // - Writing 10 additional commands
+  for (i <- 1 to 10) {
+    poke(dut.io.addr, i.U)
+    poke(dut.io.wrData, (dummyCmd + i).U)
+    step(1)
+  }
+  // - Expecting active valid bits for all of them (0 to 10)
+  for (i <- 0 until dut.getDataW)
+    expect(dut.io.cmdValid(i), (i<=10))
+
+  // Starting FSM and inspecting it as it "processes" the commands
+  // - startWord: Sets chip enable to start operations
+  val startWord = (1 << dut.chEn)
+  poke(dut.io.commandSel, false.B)
+  poke(dut.io.statusSel, true.B)
+  poke(dut.io.wrData, startWord.U)
+  expect(dut.io.stateReg, dut.idle)     // FSM starts from idle state
+  step(1)
+  poke(dut.io.statusSel, false.B)
+  poke(dut.io.rdWrN, true.B)            // FSM is still in idle state because signal propagation
+  expect(dut.io.stateReg, dut.idle)     // takes two cycles: io.wrData -> statusReg -> stateReg
+  step(1)
+  expect(dut.io.stateReg, dut.fetch)    // FSM is in fetch state after one clock edge
+  step(1)
+  expect(dut.io.stateReg, dut.decode)   // FSM is in decode state after two clock edges
+  expect(dut.io.currCommand, dummyCmd)  // 0th command is in current-command register
+  expect(dut.io.cmdPtr, 1.U)            // cmdPtr points to the next (1st) command
+  step(1)
+  expect(dut.io.stateReg, dut.execute)  // FSM is in execute state after three clock edges
+  expect(dut.io.currCommand, dummyCmd)  // Current-command register remains the same
+  expect(dut.io.cmdPtr, 1.U)            // cmdPtr remains the same
+  // - This repeats until no valid command remains
+  var currCmd = dummyCmd
+  var cmdPtr = 1
+  while (toBoolean(peek(dut.io.cmdValid(cmdPtr)), 0)) {
+    step(1)
+    expect(dut.io.stateReg, dut.fetch)
+    currCmd += 1  // Modelling operations
+    cmdPtr += 1   // in fetch state
+    step(1)
+    expect(dut.io.stateReg, dut.decode)
+    expect(dut.io.currCommand, currCmd)
+    expect(dut.io.cmdPtr, cmdPtr)
+    step(1)
+    expect(dut.io.stateReg, dut.execute)  // FSM is in execute state after three clock edges
+    expect(dut.io.currCommand, currCmd)   // Current-command register remains the same
+    expect(dut.io.cmdPtr, cmdPtr)         // cmdPtr remains the same
+  }
+  step(1)
+  expect(dut.io.stateReg, dut.idle)  // First, the FSM goes to idle
+  step(10)
+  val expStatus = (1 << dut.chEn) + (1 << dut.qEmp)
+  expect(dut.io.statusReg, expStatus)  // Then queue-empty bit gets asserted
+
+
+  // Helper functions
+  def toBoolean(in: BigInt, bitPos: Int) = ((in >> bitPos) % 2) == 1
 }
