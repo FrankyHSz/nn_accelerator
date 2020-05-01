75063846e66904be0a5e713dce55d21435783686
Szabolcs Hornyák <sztarkantal@gmail.com>
2020-05-01 22:50:38 +0200

    Modified LoadUnit to generate addresses for matrix multiplication and convolution

--- src/main/scala/memory/LoadUnit.scala
+++ src/main/scala/memory/LoadUnit.scala
@@ -7,14 +7,17 @@ class LoadUnit extends Module {
   val io = IO(new Bundle() {
 
     // Control interface
-    val en = Input(Bool())
+    val en       = Input(Bool())
+    val mulConvN = Input(Bool())
+    val sizeA    = Input(UInt(localAddrWidth.W))
+    val sizeB    = Input(UInt(localAddrWidth.W))
 
     // Local memory interfaces
     val addrA = Output(UInt(localAddrWidth.W))
     val addrB = Output(UInt(localAddrWidth.W))
 
     // Arithmetic Grid interface
-    val auEn  = Output(Bool())
+    val auEn  = Output(Vec(gridSize, Bool()))
     val auClr = Output(Bool())
   })
 
@@ -22,8 +25,28 @@ class LoadUnit extends Module {
   val addrAReg = RegInit(0.U(localAddrWidth.W))
   val addrBReg = RegInit(0.U(localAddrWidth.W))
   when (io.en) {
-    addrAReg := addrAReg + gridSize.U
-    addrBReg := addrBReg + gridSize.U
+
+    // Generating addresses for matrix multiplication
+    when (io.mulConvN) {
+      addrAReg := addrAReg + 1.U
+      addrBReg := addrBReg + gridSize.U
+
+    // Generating addresses for convolution
+    } .otherwise {
+      // When reaching the end of the kernel, we reset the address
+      when (addrAReg === (io.sizeA*io.sizeA)-1.U) {
+        addrAReg := 0.U
+      } .otherwise {
+        addrAReg := addrAReg + 1.U
+      }
+      // When we reach the right edge of image with
+      // right edge of kernel then we jump to the next line
+      when (addrBReg(localAddrWidth/2-1, 0) === (io.sizeB - io.sizeA - 1.U)) {
+        addrBReg := addrBReg + io.sizeA + 1.U
+      } .otherwise {
+        addrBReg := addrBReg + 1.U
+      }
+    }
   } .otherwise {
     addrAReg := 0.U
     addrAReg := 0.U
@@ -31,8 +54,24 @@ class LoadUnit extends Module {
 
   io.addrA := addrAReg
   io.addrB := addrBReg
-  io.auEn  := RegNext(io.en)
-  io.auClr := RegNext(io.en && (addrAReg === 0.U))  // Clearing AU accumulators after wrap-around
+
+  for (i <- 0 until gridSize) {
+
+    // For multiplication, as many AUs are enabled as the width of Matrix B
+    when (io.mulConvN) {
+      io.auEn(i) := RegNext(io.en) && RegNext(i.U < io.sizeB)
+
+    // For convolution, as many AUs are enabled
+    // as the number of kernels can fit into Matrix B
+    } .otherwise {
+      io.auEn(i) := RegNext(io.en) && RegNext(i.U < (io.sizeB - io.sizeA - 1.U))
+    }
+  }
+
+  // Clearing AU accumulators after wrap-around
+  // Mul: Address become 0 when a row of the output matrix is finished
+  // Conv: Address become 0 when one row of output pixels are finished
+  io.auClr := RegNext(io.en && (addrAReg === 0.U))
 
   def getAddrW = localAddrWidth
   def getN = gridSize
--- src/test/scala/memory/LoadUnitTester.scala
+++ src/test/scala/memory/LoadUnitTester.scala
@@ -8,12 +8,50 @@ class LoadUnitTester(dut: LoadUnit) extends PeekPokeTester(dut) {
   val maxAddress = 1 << dut.getAddrW
   val n = dut.getN
 
+  // Testing addresses for matrix multiplication
+  println("[LoadUnit] Testing addresses for matrix multiplication")
   poke(dut.io.en, true.B)
-  for (i <- 0 until 2*maxAddress by n) {
-    expect(dut.io.addrA, i % maxAddress)
-    expect(dut.io.addrB, i % maxAddress)
-    expect(dut.io.auEn, i > 0)
-    expect(dut.io.auClr, (i % maxAddress) == n)
+  poke(dut.io.mulConvN, true.B)
+  poke(dut.io.sizeA, n.U)
+  poke(dut.io.sizeB, n.U)
+  step(1)
+  for (t <- 1 until 2*maxAddress) {
+    expect(dut.io.addrA, t % maxAddress)
+    expect(dut.io.addrB, (t * n) % maxAddress)
+    for (i <- 0 until n)
+      expect(dut.io.auEn(i), true)
+    expect(dut.io.auClr, ((t-1) % maxAddress) == 0)
     step(1)
   }
+
+  // Resetting address registers
+  poke(dut.io.en, false.B)
+  step(1)
+
+  // Testing addresses for convolution
+  println("[LoadUnit] Testing addresses for convolution")
+
+  // Testing for all supported kernel sizes
+  print("Tested kernel sizes: ")
+  for (kernelSize <- 3 to 8) {
+    print(kernelSize + " ")
+    poke(dut.io.en, true.B)
+    poke(dut.io.mulConvN, false.B)
+    poke(dut.io.sizeA, kernelSize.U)
+    poke(dut.io.sizeB, n.U)
+    step(1)
+    for (t <- 1 until 2 * (n * (n-kernelSize))) {
+      expect(dut.io.addrA, t % (kernelSize*kernelSize))
+      expect(dut.io.addrB, ((t/(n-kernelSize))*n + t%(n-kernelSize)) % maxAddress)
+      for (i <- 0 until n)
+        expect(dut.io.auEn(i), (i < (n-kernelSize-1)))
+      expect(dut.io.auClr, ((t-1) % (kernelSize*kernelSize)) == 0)
+      step(1)
+    }
+
+    // Resetting address registers
+    poke(dut.io.en, false.B)
+    step(1)
+  }
+  print("\n")
 }
